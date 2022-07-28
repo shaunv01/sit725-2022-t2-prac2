34bf97ea4a45f488dd56a9a60d34d0c5fc7a7f4d
shaunv01 <svorster@deakin.edu.au>
2022-07-28 19:43:18 +1000

    Final upload

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # sit725-2022-t2-prac2
 2022 trimester 2 practical 2
+
+Express JS is required to run this
+
+Application can be run using: "node server.js", alternatively we can use "nodemon server.js" to automatically restart and run the application when the server script changes. 
+This is done by adding the nodemon statement in a script called "start" to the package.json scripts and then running "npm run start". VS Code does also have the run & debug option.
+
+A "get" method has been implemented to add 2 number together, this can be called from a brower using the url:
+localhost:3030/addTwoNumber?n1=10&n2=15
+
+A "post" method has been implement to add 2 number together, using postman, we can pass json in the body of the request and we will get the same reult.
+
+A book database in json has been created. There are get and push functions.
+The get function will allow for a price selection, so if "p" is specified then the all the books less than or equal to that price will be returned.
+The post method will allow a new book to be added, provided that the book does not have a title which already exists.
+
+The put method has been implemented to update the "author" or the "price" of the book. This put method uses a query string as input, but there is also code commented out which can use the body json title value.
+The delete method has been implement to remove books by title. The title is passed in the body as json. It could be a query variable though, but I have used the body.
--- server.js
+++ server.js
@@ -1,15 +1,175 @@
-const express = require('express');
+// Include express JS library
+const express = require("express");
+// create instance of express library
 const app = express();
 
 app.use(express.json());
 
+const books = [
+    {
+        title: "Book 1",
+        author: "author 1",
+        price: 200
+    },
+    {
+        title: "Book 2",
+        author: "author 2",
+        price: 150
+    },
+    {
+        title: "Book 3",
+        author: "author 3",
+        price: 50
+    },
+    {
+        title: "Book 4",
+        author: "author 4",
+        price: 105
+    },
+    {
+        title: "Book 5",
+        author: "author 5",
+        price: 25
+    }
+];
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
+const bookExists = (myTitle) => {
+    for (var i = 0; i < books.length; i++) {
+        if (myTitle == books[i].title) {
+            return true;
+        }
+    }
+};
+
+app.get("/book", (req, res) => {
+    try{
+        if(req.query.p != null) {
+            res.json({
+                statusCode: 200, data: books.filter(x => x.price <= parseInt(req.query.p))
+            });
+        }
+        res.json({ statusCode:200, data:books });
+        
+    }catch(e){
+        res.json({ statusCode:500, data: "something went wrong with get" });
+    }
 });
 
-var port = process.env.port || 3000;
-
-app.listen(port, ()=>{
-  console.log('App running at http://localhost:'+port);
+app.put("/book", (req, res) => {
+    try{
+        let bookFound = false;
+        for (var i = 0; i < books.length; i++) {
+            console.log(books[i].title + ' ' + req.body.title + ' ' + req.query.title);
+            //if (req.body.title == books[i].title) {
+            if (req.query.title == books[i].title) {
+                books[i].author = req.body.author;
+                books[i].price = req.body.price;
+                bookFound = true;
+            }
+        }
+        if (!bookFound) {
+            res.json({ statusCode: 500, data: "could not find the book with the title " + req.body.title });
+        } else {
+            res.json({ statusCode:200, data:books });
+        }
+        
+    }catch(e){
+        res.json({ statusCode:500, data: "something went wrong with get" });
+    }
 });
+
+app.delete("/book", (req, res) => {
+    try{
+        let bookFound = false;
+        for (var i = 0; i < books.length; i++) {
+            console.log(books[i].title);
+            if (req.body.title == books[i].title) {
+                delete books[i];
+                bookFound = true;
+            }
+        }
+        if (!bookFound) {
+            res.json({ statusCode: 500, data: "could not find the book with the title " + req.body.title });
+        } else {
+            res.json({ statusCode:200, data:books });
+        }
+        
+    }catch(e){
+        res.json({ statusCode:500, data: "something went wrong with get" });
+    }
+});
+
+app.post("/book", (req, res) => {
+    try{
+        //validation
+        var exists = bookExists(req.body.title);
+        if (exists) {
+            res.json({ statusCode: 500, data: "already exists" });
+        } else {
+            books.push(req.body);
+            res.json({ statusCode: 200 });
+        }
+        
+    }catch(e){
+        res.json({ statusCode: 500, data: "something went wrong with post" });
+    }
+});
+
+// console.log(addTwoNumber(1,2));
+
+const doMath = (n1, n2, op) => {
+    if (Number.isNaN(n1) || Number.isNaN(n2)){
+        throw "error";
+    }
+    if (op == "+") {
+        return n1 + n2;
+    } else if (op == "-") {
+        return n1 - n2;
+    } else if (op =="*") {
+        return n1 * n2;
+    } else if (op =="%") {
+        return n1 / n2l
+    }
+};
+
+const addTwoNumber = (n1, n2) => {
+    if (Number.isNaN(n1) || Number.isNaN(n2)) {
+        throw "error";
+    }
+    console.log("n1=" + n1 + " n2=" + n2);
+    return n1 + n2;
+};
+
+app.get("/addTwoNumbers", (req, res) => {
+    try{
+        const n1 = parseInt(req.query.n1);
+        const n2 = parseInt(req.query.n2);
+        const result = addTwoNumber(n1, n2);
+        res.json({ statusCode : 200, data: result });
+    }catch(e){
+        res.json({ statusCode : 500, data:"whoopsie get" })
+    }
+    
+});
+
+app.post("/addTwoNumbers", (req, res) => {
+    try{
+        const n1 = parseInt(req.body.n1);
+        const n2 = parseInt(req.body.n2);
+        const result = addTwoNumber(n1, n2);
+        res.json({ statusCode : 200, data: result });
+    }catch(e){
+        res.json({ statusCode : 500, data : "whoopsie post" })
+    }    
+});
+
+// setup a constant for the application port to listen on
+const port = 3030;
+
+// start the application listening on the port
+app.listen(port,() => {
+    console.log("Hello I'm listening to port " + port);
+})
+
+// console.log("Hello I'm here at line 13");
+
